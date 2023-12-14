ab88d5effa8d7d6ed9d1e3b61155aaa7f21505a8
avahak <a.vahakangas.work@gmail.com>
2023-12-14 09:56:45 +0200

    harjoitukset 14.12.23 kosmeettisia korjauksia

--- src/components/AddTodo.tsx
+++ src/components/AddTodo.tsx
@@ -8,66 +8,66 @@ import DialogContent from '@mui/material/DialogContent';
 import DialogTitle from '@mui/material/DialogTitle';
 
 function AddTodo(props: any) {
-  const [open, setOpen] = useState(false);
-  const [todo, setTodo] = useState({description: '', date: '', priority: ''});
+    const [open, setOpen] = useState(false);
+    const [todo, setTodo] = useState({description: '', date: '', priority: ''});
 
-  const handleOpen = () => {
-    setOpen(true);
-  }
+    const handleOpen = () => {
+        setOpen(true);
+    }
 
-  const handleClose = () => {
-    setOpen(false);
-  }
+    const handleClose = () => {
+        setOpen(false);
+    }
 
-  const handleSave = () => {
-    props.addTodo(todo);
-    handleClose();
-  }
+    const handleSave = () => {
+        props.addTodo(todo);
+        handleClose();
+    }
 
-  const inputChanged = (event: any) => {
-    setTodo({...todo, [event.target.name]: event.target.value});
-  }
+    const inputChanged = (event: any) => {
+        setTodo({...todo, [event.target.name]: event.target.value});
+    }
 
-  return(
-    <>
-      <Button variant="outlined" onClick={handleOpen}>
-        Add todo
-      </Button>
-     <Dialog open={open}>
-       <DialogTitle>New todo</DialogTitle>
-       <DialogContent> 
-         <TextField
-            name="description"
-            value={todo.description}
-            onChange={inputChanged}
-            margin="dense"
-            label="Description"
-            fullWidth
-          /> 
-         <TextField
-           name="date"
-           value={todo.date}
-           onChange={inputChanged}
-           margin="dense"
-           label="Date"
-           fullWidth
-         /> 
-         <TextField
-           name="priority"
-           value={todo.priority}
-           onChange={inputChanged}
-           margin="dense"
-           label="Priority"
-           fullWidth
-         /> 
-      </DialogContent>
-      <DialogActions>
-         <Button color="primary" onClick={handleClose}>Cancel</Button>
-         <Button color="primary" onClick={handleSave}>Save</Button>
-      </DialogActions>
-     </Dialog> 
-    </>
-  );
+    return(
+        <>
+            <Button variant="outlined" onClick={handleOpen}>
+                Add todo
+            </Button>
+            <Dialog open={open}>
+                <DialogTitle>New todo</DialogTitle>
+                <DialogContent> 
+                    <TextField
+                        name="description"
+                        value={todo.description}
+                        onChange={inputChanged}
+                        margin="dense"
+                        label="Description"
+                        fullWidth
+                    /> 
+                    <TextField
+                        name="date"
+                        value={todo.date}
+                        onChange={inputChanged}
+                        margin="dense"
+                        label="Date"
+                        fullWidth
+                    /> 
+                    <TextField
+                        name="priority"
+                        value={todo.priority}
+                        onChange={inputChanged}
+                        margin="dense"
+                        label="Priority"
+                        fullWidth
+                    /> 
+                </DialogContent>
+                <DialogActions>
+                    <Button color="primary" onClick={handleClose}>Cancel</Button>
+                    <Button color="primary" onClick={handleSave}>Save</Button>
+                </DialogActions>
+            </Dialog> 
+        </>
+    );
 }
 
 export default AddTodo;
--- src/components/Todo.tsx
+++ src/components/Todo.tsx
@@ -1,3 +1,4 @@
+// Viope kurssilta
 import { useState, useEffect } from 'react';
 import { AgGridReact } from 'ag-grid-react';
 import { ColDef } from 'ag-grid-community';
@@ -12,79 +13,79 @@ import 'ag-grid-community/styles/ag-grid.css';
 import 'ag-grid-community/styles/ag-theme-material.css';
 
 function Todo() {
-  const [todos, setTodos] = useState([]);
+    const [todos, setTodos] = useState([]);
 
-  const columnDefs: ColDef[] = [
-    { field: 'description', sortable: true, filter: true},
-    { field: 'date', sortable: true, filter: true},
-    { field: 'priority', sortable: true, filter: true},
-    { 
-      headerName: '',
-      field: 'id',
-      width: 90,
-      cellRenderer: (params: any) => 
-      <IconButton onClick={() => deleteTodo(params.value)} size="small" color="error">
-        <DeleteIcon />
-      </IconButton> 
+    const columnDefs: ColDef[] = [
+        { field: 'description', sortable: true, filter: true},
+        { field: 'date', sortable: true, filter: true},
+        { field: 'priority', sortable: true, filter: true},
+        { 
+            headerName: '',
+            field: 'id',
+            width: 90,
+            cellRenderer: (params: any) => 
+                <IconButton onClick={() => deleteTodo(params.value)} size="small" color="error">
+                    <DeleteIcon />
+                </IconButton> 
+        }
+    ]
+
+    useEffect(() => {
+        fetchItems();
+    }, [])
+
+    const fetchItems = () => {
+        fetch('https://todolist-3d03b-default-rtdb.europe-west1.firebasedatabase.app/items/.json')
+        .then((response: any) => response.json())
+        .then((data: any) => addKeys(data))
+        .catch((err: any) => console.error(err))
     }
-  ]
 
-  useEffect(() => {
-    fetchItems();
-  }, [])
-
-  const fetchItems = () => {
-    fetch('https://todolist-3d03b-default-rtdb.europe-west1.firebasedatabase.app/items/.json')
-    .then((response: any) => response.json())
-    .then((data: any) => addKeys(data))
-    .catch((err: any) => console.error(err))
-  }
-
-  // Add keys to the todo objects
-  const addKeys = (data: any) => {
-    const keys = Object.keys(data);
-    const valueKeys = Object.values(data).map((item, index) => 
-    Object.defineProperty(item, 'id', {value: keys[index]}));
-    setTodos(valueKeys as any);
-  }
+    // Add keys to the todo objects
+    const addKeys = (data: any) => {
+        const keys = Object.keys(data);
+        const valueKeys = Object.values(data).map((item, index) => 
+        Object.defineProperty(item, 'id', {value: keys[index]}));
+        setTodos(valueKeys as any);
+    }
    
-  const addTodo = (newTodo: any) => {
-    fetch('https://todolist-3d03b-default-rtdb.europe-west1.firebasedatabase.app/items/.json',
-    {
-      method: 'POST',
-      body: JSON.stringify(newTodo)
-    })
-    .then(_response => fetchItems())
-    .catch(err => console.error(err))
-  }
+    const addTodo = (newTodo: any) => {
+        fetch('https://todolist-3d03b-default-rtdb.europe-west1.firebasedatabase.app/items/.json',
+        {
+            method: 'POST',
+            body: JSON.stringify(newTodo)
+        })
+        .then(_response => fetchItems())
+        .catch(err => console.error(err))
+    }
 
-  const deleteTodo = (id: number) => {
-    fetch(`https://todolist-3d03b-default-rtdb.europe-west1.firebasedatabase.app/items/${id}.json`,
-    {
-      method: 'DELETE',
-    })
-    .then(_response => fetchItems())
-    .catch(err => console.error(err))
-  }
+    const deleteTodo = (id: number) => {
+        fetch(`https://todolist-3d03b-default-rtdb.europe-west1.firebasedatabase.app/items/${id}.json`,
+        {
+            method: 'DELETE',
+        })
+        .then(_response => fetchItems())
+        .catch(err => console.error(err))
+    }
 
-  return (
-    <div style={{ display: "block" }}>
-      <AppBar position="static">
-        <Toolbar>
-          <Typography variant="h5">
-            TodoList
-          </Typography>
-        </Toolbar>
-      </AppBar> 
-      <AddTodo addTodo={addTodo} />
-      <div className="ag-theme-material" style={{ height: 400, width: 700 }}>
-        <AgGridReact 
-          rowData={todos}
-          columnDefs={columnDefs}
-        />
-      </div>
-    </div>
-  );
+    return (
+        <div style={{ display: "block" }}>
+            <AppBar position="static">
+                <Toolbar>
+                    <Typography variant="h5">
+                        TodoList
+                    </Typography>
+                </Toolbar>
+            </AppBar> 
+            <AddTodo addTodo={addTodo} />
+            <div className="ag-theme-material" style={{ height: 400, width: 700 }}>
+                <AgGridReact 
+                    rowData={todos}
+                    columnDefs={columnDefs}
+                />
+            </div>
+        </div>
+    );
 }
 
 export default Todo;
